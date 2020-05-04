079b6dcbd7f37956a01da7120c6757a55b94b59e
capac <capac@users.noreply.github.com>
2020-05-04 13:03:03 +0100

    Histogram of journeys per bike per day

--- bicycle_data_analysis.py
+++ bicycle_data_analysis.py
@@ -12,22 +12,25 @@ data_dir = Path(home) / 'Programming/data/s2ds-project-data'
 # connect to SQLite DB on laptop
 flow_journey_db = data_dir / 'FlowJourneyData.db'
 con = sqlite3.connect(flow_journey_db)
-cursor = con.execute('''SELECT Bike_Id, End_Date, Start_Date FROM journeys''')
+cursor = con.execute('''SELECT Bike_Id, Rental_Id, End_Date, Start_Date FROM journeys''')
 
 journey_results = cursor.fetchall()
 bike_df = pd.DataFrame(journey_results, columns = [x[0] for x in cursor.description])
+# print(bike_df.head())
 for col in ['End_Date', 'Start_Date']:
     bike_df[col] = pd.to_datetime(bike_df[col], format=r'%Y-%m-%d %H:%M:%S%f', errors='raise', exact=False)
 
 max_min_days = bike_df['End_Date'].max() - bike_df['Start_Date'].min()
 total_duration = max_min_days.total_seconds()/(24*3600)
 
-grouped_bikes_journeys_per_day = bike_df.groupby('Bike_Id')['Bike_Id'].count()/(total_duration)
+grouped_bikes_and_journeys = bike_df['Rental_Id'].groupby(bike_df['Bike_Id'])
+grouped_journeys_per_bikes_per_day = grouped_bikes_and_journeys.count()/total_duration
 
-# print(grouped_bikes_journeys_per_day.head(20))
+gtr_df = grouped_journeys_per_bikes_per_day.loc[grouped_journeys_per_bikes_per_day >= 0.5]
+# print(gtr_df.sort_values(ascending=False))
 
 fig, axes = plt.subplots(figsize=(8, 6))
-axes.hist(grouped_bikes_journeys_per_day, 30, alpha=0.8, edgecolor='k', color='firebrick')
+axes.hist(gtr_df, 50, alpha=0.8, edgecolor='k', color='red')
 axes.set_xlabel('Number of journeys per bike per day')
 plt.grid(linestyle=':')
 print(f'Time elapsed: {time() - t0:.2f} seconds')
