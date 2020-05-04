9032437ff5d0e0088ead7ba22e2e052e1c762ee1
capac <capac@users.noreply.github.com>
2020-05-04 18:34:56 +0100

    A bit of cleanup, important code is the same

--- journeys.py
+++ journeys.py
@@ -12,7 +12,7 @@ data_dir = Path(home) / 'Programming/data/s2ds-project-data'
 # connect to SQLite DB on laptop
 flow_journey_db = data_dir / 'FlowJourneyData.db'
 con = sqlite3.connect(flow_journey_db)
-cursor = con.execute('''SELECT Bike_Id, COUNT(Rental_Id) AS Num_Journeys, SUM(Duration) AS Tot_Time FROM journeys GROUP BY Bike_Id HAVING Tot_Time > 1200''')
+cursor = con.execute('''SELECT Bike_Id, COUNT(Rental_Id) AS Num_Journeys, SUM(Duration) AS Tot_Time FROM journeys GROUP BY Bike_Id HAVING Tot_Time > 0''')
 
 journey_results = cursor.fetchall()
 bike_df = pd.DataFrame(journey_results, columns = [x[0] for x in cursor.description])
@@ -20,11 +20,8 @@ bike_df = pd.DataFrame(journey_results, columns = [x[0] for x in cursor.descript
 #     bike_df[col] = pd.to_datetime(bike_df[col], format=r'%Y-%m-%d %H:%M:%S%f', errors='raise', exact=False)
 
 # print(bike_df.head(10))
-# print(bike_df.sort_values(by='count', ascending=False)[:10])
+# print(bike_df.sort_values(by='Tot_Time', ascending=False)[-10:])
 # print(bike_df.isna().any())
-
-# max_min_days = bike_df['End_Date'].max() - bike_df['Start_Date'].min()
-# total_duration = max_min_days.total_seconds()/(24*3600)
 
 # grouped_bikes_and_journeys = bike_df['Rental_Id'].groupby(bike_df['Bike_Id'])
 # grouped_journeys_per_bikes_per_day = grouped_bikes_and_journeys.count()/total_duration
@@ -32,9 +29,6 @@ bike_df = pd.DataFrame(journey_results, columns = [x[0] for x in cursor.descript
 bike_df['Journeys_Time'] = (3600*24)*bike_df['Num_Journeys'].divide(bike_df['Tot_Time'])
 
 # print(np.argwhere(np.isnan(bike_df['Num_Journeys'].values)))
-
-# gtr_df = grouped_journeys_per_bikes_per_day.loc[grouped_journeys_per_bikes_per_day >= 0.5]
-# print(gtr_df.sort_values(ascending=False))
 
 fig, axes = plt.subplots(figsize=(8, 6))
 axes.hist(bike_df['Journeys_Time'], 60, range=[0, 150], alpha=0.8, edgecolor='k', color='red')
