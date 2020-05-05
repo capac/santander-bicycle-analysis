34ce0ad5c616e682a55a60d5cc25a1343439a21c
capac <capac@users.noreply.github.com>
2020-05-05 12:55:19 +0100

    Removed commented code

--- journeys.py
+++ journeys.py
@@ -13,26 +13,16 @@ data_dir = Path(home) / 'Programming/data/s2ds-project-data'
 flow_journey_db = data_dir / 'FlowJourneyData.db'
 con = sqlite3.connect(flow_journey_db)
 cursor = con.execute('''SELECT Bike_Id, COUNT(Rental_Id) AS Num_Journeys, SUM(Duration) AS Tot_Time FROM journeys GROUP BY Bike_Id HAVING Tot_Time > 0''')
-
 journey_results = cursor.fetchall()
+
+# pandas dataframe
 bike_df = pd.DataFrame(journey_results, columns = [x[0] for x in cursor.description])
-# for col in ['End_Date', 'Start_Date']:
-#     bike_df[col] = pd.to_datetime(bike_df[col], format=r'%Y-%m-%d %H:%M:%S%f', errors='raise', exact=False)
+bike_df['Journeys_Time'] = bike_df['Num_Journeys'].divide(bike_df['Tot_Time']/(24*3600))
 
-# print(bike_df.head(10))
-# print(bike_df.sort_values(by='Tot_Time', ascending=False)[-10:])
-# print(bike_df.isna().any())
-
-# grouped_bikes_and_journeys = bike_df['Rental_Id'].groupby(bike_df['Bike_Id'])
-# grouped_journeys_per_bikes_per_day = grouped_bikes_and_journeys.count()/total_duration
-
-bike_df['Journeys_Time'] = (3600*24)*bike_df['Num_Journeys'].divide(bike_df['Tot_Time'])
-
-# print(np.argwhere(np.isnan(bike_df['Num_Journeys'].values)))
-
+# histogram
 fig, axes = plt.subplots(figsize=(8, 6))
-axes.hist(bike_df['Journeys_Time'], 60, range=[0, 150], alpha=0.8, edgecolor='k', color='red')
-axes.set_xlabel('Number of Journeys per bike per day')
+axes.hist(bike_df['Journeys_Time'], 100, range=[0, 150], alpha=0.8, edgecolor='k', color='red')
+axes.set_xlabel('Number of journeys per bike per day')
 plt.grid(linestyle=':')
 print(f'Time elapsed: {time() - t0:.2f} seconds')
 fig.tight_layout()
