5ee756ee905586554aafe8ce38653059b1308f69
capac <capac@users.noreply.github.com>
2020-05-05 18:34:04 +0100

    Created SQLite table 'Averages' and histogram from query to table

--- journeys.py
+++ journeys.py
@@ -12,16 +12,19 @@ data_dir = Path(home) / 'Programming/data/s2ds-project-data'
 # connect to SQLite DB on laptop
 flow_journey_db = data_dir / 'FlowJourneyData.db'
 con = sqlite3.connect(flow_journey_db)
-cursor = con.execute('''SELECT Bike_Id, COUNT(Rental_Id) AS Num_Journeys, SUM(Duration) AS Tot_Time FROM journeys GROUP BY Bike_Id HAVING Tot_Time > 0''')
+query = '''SELECT Bike_Id, Sum_Journeys, Count_Journeys, Average FROM Averages'''
+# query = '''SELECT Bike_Id, SUM(Num_Journeys) AS Sum_Journeys, COUNT(Num_Journeys) AS Count_Journeys, ROUND(1.0*SUM(Num_Journeys)/COUNT(Num_Journeys), 2) AS Average FROM (SELECT strftime("%Y-%m-%d", End_Date) AS Day, Bike_Id, COUNT(Rental_Id) AS Num_Journeys, SUM(Duration) AS Tot_Time FROM journeys GROUP BY Day, Bike_Id HAVING Tot_Time > 0) GROUP BY Bike_Id'''
+cursor = con.execute(query)
 journey_results = cursor.fetchall()
 
 # pandas dataframe
 bike_df = pd.DataFrame(journey_results, columns = [x[0] for x in cursor.description])
-bike_df['Journeys_Time'] = bike_df['Num_Journeys'].divide(bike_df['Tot_Time']/(24*3600))
+# print(bike_df.head(20))
 
 # histogram
 fig, axes = plt.subplots(figsize=(8, 6))
-axes.hist(bike_df['Journeys_Time'], 100, range=[0, 150], alpha=0.8, edgecolor='k', color='red')
+axes.hist(bike_df['Average'], 60, range=[2, 6], alpha=0.8, edgecolor='k', color='red')
+axes.vlines(bike_df['Average'].mean(), axes.yaxis.get_data_interval()[0], axes.yaxis.get_data_interval()[1], linestyles=':')
 axes.set_xlabel('Number of journeys per bike per day')
 plt.grid(linestyle=':')
 print(f'Time elapsed: {time() - t0:.2f} seconds')
