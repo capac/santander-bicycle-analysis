e128a9276e264074c16ccec66f69865428542464
capac <capac@users.noreply.github.com>
2020-05-17 10:19:57 +0100

    Added title and plot labels

--- rides.py
+++ rides.py
@@ -4,7 +4,7 @@ import os
 import sqlite3
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
-from datetime import datetime
+from datetime import datetime, timedelta
 from pathlib import Path
 from time import time
 
@@ -33,13 +33,16 @@ filter_ = bike_df['Day'].isin(dates)
 days = bike_df['Day'].loc[filter_]
 rides = bike_df['Sum_Journeys'].loc[filter_]
 fig, axes = plt.subplots(figsize=(8, 6))
-axes.scatter(days, rides, alpha=0.6, marker='o',
+axes.scatter(days, rides/1e4, alpha=0.6, marker='o',
              color='cornflowerblue', edgecolor='k')
 
 # London tube strikes, in tuples with years, months and days
-for dt in [(2012, 12, 26), (2014, 2, 4), (2014, 2, 5), (2014, 2, 6), (2014, 4, 28), (2014, 4, 29), (2014, 4, 30), (2014, 8, 22), (2014, 8, 23), (2014, 12, 1), (2015, 7, 8), (2015, 7, 9), (2015, 8, 5), (2015, 8, 6)]:
+# london_tube_strike_List =  [(2012, 12, 26), (2014, 2, 4), (2014, 2, 5), (2014, 2, 6), (2014, 4, 28), (2014, 4, 29), (2014, 4, 30), (2014, 8, 22), (2014, 8, 23), (2014, 12, 1), (2015, 7, 8), (2015, 7, 9), (2015, 8, 5), (2015, 8, 6)]
+for dt in [(2012, 12, 26), (2014, 2, 6), (2014, 4, 30), (2014, 8, 23), (2014, 12, 1), (2015, 7, 9), (2015, 8, 6)]:
     axes.vlines(datetime(*dt), axes.yaxis.get_data_interval()
-                [0], axes.yaxis.get_data_interval()[-1], linestyle=':', label='Tube strike')
+                [0], axes.yaxis.get_data_interval()[-1], linestyle=':')
+    axes.annotate('Tube strike', xy=(datetime(*dt)-timedelta(20), axes.yaxis.get_data_interval()
+                                     [0] + 5), ha='left', va='bottom', rotation=90, size=7, color='k')
 
 # format the ticks
 months = mdates.MonthLocator()  # every month
@@ -61,9 +64,11 @@ axes.set_xlim(datemin, datemax)
 fig.autofmt_xdate()
 
 # set axes labels
-axes.set_xlabel('Rides per day')
+axes.set_xlabel('Dates')
+axes.set_ylabel('Rides per day (in units of 10,000)')
+axes.set_title(
+    f'Number of rides per day from {days.min():%Y-%m-%d} to {days.max():%Y-%m-%d}')
 fig.tight_layout()
 axes.grid(linestyle=':')
-# plt.legend(loc='best')
 print(f'Time elapsed: {time() - t0:.2f} seconds')
 plt.show()
