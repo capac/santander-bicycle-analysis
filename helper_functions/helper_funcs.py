b6b5d7cb1f8cd8a66a80fdbcfd4192770a5c3f96
capac <capac@users.noreply.github.com>
2020-06-03 18:16:21 +0100

    Added time lapse video feature

--- helper_functions/helper_funcs.py
+++ helper_functions/helper_funcs.py
@@ -5,8 +5,9 @@ from itertools import islice
 import os
 import pandas as pd
 import numpy as np
-from bokeh.models import ColumnDataSource
+from bokeh.models import ColumnDataSource, Button
 from bokeh.models.widgets import Slider, Select
+from bokeh.io import curdoc
 
 
 # load data from CSV file
@@ -16,24 +17,28 @@ avg_weekdays_sum_diff_df = pd.read_csv(data_dir /
                                        'avg_weekday_sum_diff_merc_coord.csv',
                                        index_col='Date')
 
+
 # group elements of list in tuples of four elements each for
 # sum, difference, latitude and longitude (in Mercator coords)
-
-
 def station_chunk(it, size):
     it = iter(it)
     return iter(lambda: tuple(islice(it, size)), ())
 
 
+# scale factor for scale visualization of data points
+scale_factor = 1e2
+
 # return a list of dictionaries, each one of which has time as key and
 # a dictionary as value with sum_flux, diff_flux, lat and long values
+
+
 def bike_flux(flux_df):
     bike_flux_list = []
     for row in flux_df.itertuples():
         sum_flux, diff_flux, lat, long = list(
             map(tuple, zip(*station_chunk(row[1:], 4))))
-        sum_flux = np.array(sum_flux)/1e2
-        diff_flux = np.array(diff_flux)/1e2
+        sum_flux = np.array(sum_flux)/scale_factor
+        diff_flux = np.array(diff_flux)/scale_factor
         bike_flux_list.append(
             {row[0]: {'sum_flux': sum_flux, 'diff_flux': diff_flux,
                       'lat': lat, 'long': long}})
@@ -46,25 +51,11 @@ bike_flux_list = bike_flux(avg_weekdays_sum_diff_df)
 time_interval_dict = {time_interval: bike_flux_list.index(interval_data) for interval_data
                       in bike_flux_list for time_interval in interval_data.keys()}
 
-# drop down hour selector
-hour_interval_selector = Select(title='Hour interval',
-                                options=list(time_interval_dict.keys()),
-                                value='00:00:00')
-hour_interval_selector.on_change('value', lambda attr, old, new: update())
-
-# minimum traffic flux slider
-flux_slider = Slider(start=0,
-                     end=40000,
-                     value=0,
-                     step=10,
-                     title='Minimum Total Traffic (in units of hundreds)')
-flux_slider.on_change('value', lambda attr, old, new: update())
-
 
 def select_time():
     selected = bike_flux_list
     hour_interval = time_interval_dict[hour_interval_selector.value]
-    min_flux = flux_slider.value/1e2
+    min_flux = flux_slider.value/scale_factor
 
     selected_df = pd.DataFrame(
         selected[hour_interval][hour_interval_selector.value])
@@ -90,4 +81,50 @@ def update():
         lat=df['lat'],
         sum_flux=df['sum_flux'],
         diff_flux=df['diff_flux']
-        )
+    )
+
+
+def animate_update():
+    time_interval_list = list(time_interval_dict.keys())
+    next_time_interval = time_interval_list[time_interval_list.index(
+        hour_interval_selector.value) + 1]
+    if next_time_interval == time_interval_list[-1]:
+        next_time_interval = time_interval_list[0]
+    hour_interval_selector.value = next_time_interval
+
+
+callback_id = None
+
+
+def animate():
+    global callback_id
+    if button.label == '► Play':
+        button.label = '❚❚ Pause'
+        callback_id = curdoc().add_periodic_callback(animate_update, 350)
+    else:
+        button.label = '► Play'
+        curdoc().remove_periodic_callback(callback_id)
+
+
+# drop down hour selector
+hour_interval_selector = Select(title='Hour interval',
+                                options=list(time_interval_dict.keys()),
+                                value='00:00:00')
+hour_interval_selector.on_change('value', lambda attr, old, new: update())
+
+# minimum traffic flux slider
+flux_slider = Slider(start=0,
+                     end=40000,
+                     value=0,
+                     step=10,
+                     width=100,
+                     align='start',
+                     title='Lower Limit to Traffic Flux (in units of hundreds)')
+flux_slider.on_change('value', lambda attr, old, new: update())
+
+button = Button(label='► Play',
+                width=60,
+                sizing_mode='scale_width',
+                name='Traffic Time Lapse',
+                align='end')
+button.on_click(animate)
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from bokeh.tile_providers import get_provider
 from bokeh.layouts import layout, widgetbox
 from bokeh.io import curdoc
 from coordinate_transformation.to_web_merc import toWebMerc
-from helper_functions.helper_funcs import flux_slider, hour_interval_selector, source, update
+from helper_functions.helper_funcs import flux_slider, hour_interval_selector, source, update, animate, button
 
 # bokeh output HTML file
 tile_provider = get_provider('CARTODBPOSITRON')
@@ -20,46 +20,53 @@ london_y_range = (51.436, 51.568)
 merc_lower_left = toWebMerc(london_x_range[0], london_y_range[0])
 merc_upper_right = toWebMerc(london_x_range[1], london_y_range[1])
 
-tooltips = [('Total traffic', '@sum_flux{0,0.00}'), ('Net flux', '@diff_flux{0,0.00}')]
+tooltips = [
+    ('Total traffic', '@sum_flux{0,0.00}'), ('Net flux', '@diff_flux{0,0.00}')]
+
+toolbox = ['pan', 'box_zoom', 'box_select', 'hover', 'save', 'reset']
 
 # update data to show initial data point on plot
 update()
 
 # range bounds supplied in web mercator coordinates
-p = figure(x_range=(merc_lower_left[0], merc_upper_right[0]),
-           y_range=(merc_lower_left[1], merc_upper_right[1]),
-           x_axis_type="mercator",
-           y_axis_type="mercator",
-           title='Average Daily Public Bicycle Traffic in London UK',
-           toolbar_location=None,
-           tooltips=tooltips,
-           width=950,
-           height=750)
-p.add_tile(tile_provider)
-p.add_tools(HoverTool(tooltips=tooltips))
+plot = figure(x_range=(merc_lower_left[0], merc_upper_right[0]),
+              y_range=(merc_lower_left[1], merc_upper_right[1]),
+              x_axis_type="mercator",
+              y_axis_type="mercator",
+              title='Average Daily Public Bicycle Traffic in London UK',
+              toolbar_location='below',
+              tools=toolbox,
+              tooltips=tooltips,
+              toolbar_sticky=False,
+              width=950,
+              height=750)
+plot.add_tile(tile_provider)
+plot.add_tools(HoverTool(tooltips=tooltips))
 
 color_mapper = LinearColorMapper(palette='Turbo256',
                                  low=-6e2,
-                                 high=6e2)
+                                 high=5e2)
 
 color_bar = ColorBar(color_mapper=color_mapper,
                      label_standoff=12,
                      border_line_color=None,
-                     location=(0,0))
+                     location=(0, 0))
 
-p.add_layout(color_bar, 'right')
+plot.add_layout(color_bar, 'right')
 
-p.circle(x='long',
-         y='lat',
-         size='sum_flux',
-         fill_color={'field': 'diff_flux', 'transform': color_mapper},
-         fill_alpha=0.5,
-         source=source)
+plot.circle(x='long',
+            y='lat',
+            size='sum_flux',
+            fill_color={'field': 'diff_flux', 'transform': color_mapper},
+            fill_alpha=0.5,
+            source=source)
 
-hour_interval_selector_input = widgetbox(hour_interval_selector)
-flux_slider_input = widgetbox(flux_slider)
+hour_inputs = widgetbox(hour_interval_selector, sizing_mode='scale_width')
+slider_input = widgetbox(flux_slider, sizing_mode='scale_width')
 
-plot_layout = layout([[homepage], [p], [hour_interval_selector_input, flux_slider_input]])
+
+plot_layout = layout(
+    [[homepage], [plot], [hour_inputs, slider_input, button]])
 
 
 curdoc().add_root(plot_layout)
