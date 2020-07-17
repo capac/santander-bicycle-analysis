6da2aa561af51ed12c469a9d5ae856df6f86929b
capac <capac@users.noreply.github.com>
2020-07-17 09:40:17 +0100

    Updated DB file name

--- exploratory_data_analysis/rides_by_month.py
+++ exploratory_data_analysis/rides_by_month.py
@@ -8,7 +8,7 @@ import os, sqlite3
 
 home = os.environ['HOME']
 data_dir = Path(home) / r'Programming/data/s2ds-project-data'
-con = sqlite3.connect(data_dir / 'FlowJourneyData.db')
+con = sqlite3.connect(data_dir / 'journey-data_2012-2016.db')
 query_wkday = ''' SELECT strftime("%m", End_Date) AS Month, 
                          COUNT(Rental_Id) AS WeekDay_Rides
                     FROM Journeys
@@ -20,8 +20,11 @@ query_wkend = ''' SELECT strftime("%m", End_Date) AS Month,
                    WHERE strftime("%w", End_Date) IN ("0", "6")
                 GROUP BY Month'''
 query_results = [(con.execute(cur), con.execute(cur).fetchall()) for cur in [query_wkday, query_wkend]]
+print(query_results[0][0].description, query_results[0][1])
 results_df_list = [pd.DataFrame(result[1], columns=[x[0] for x in result[0].description]) for result in query_results]
+
 results_df = pd.concat(results_df_list, axis=1)
+
 rides_results_df = results_df[['WeekEnd_Rides', 'WeekDay_Rides']].rename(columns={'WeekEnd_Rides': 'Weekend', 'WeekDay_Rides': 'Weekday'}, index={0: 'Jan', 1: 'Feb', 2: 'Mar', 3: 'Apr', 4: 'May', 5: 'Jun', 6: 'Jul', 7: 'Aug', 8: 'Sep', 9: 'Oct', 10: 'Nov', 11: 'Dec'})
 
 # plot
